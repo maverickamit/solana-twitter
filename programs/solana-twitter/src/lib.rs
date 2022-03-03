2c44e01c7855a44bf9fcbe260858390972fb6360
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-03 13:01:25 +0530

    feat: update deployed program id - initial test running successfully

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("EGqjPAXfu96K6k5tXWFJGBNREesNxsGYAZTDvb8pCADz");
 
 #[program]
 pub mod solana_twitter {
