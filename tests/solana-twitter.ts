a078aad9551276af2448a18a53de78ba19708d78
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-06 13:53:19 +0530

    refactor: use prettify to format code

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -8,79 +8,79 @@ describe("solana-twitter", () => {
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
-  let tweet:anchor.web3.Keypair;
-  beforeEach( async()=>{
-     tweet = anchor.web3.Keypair.generate();
+  let tweet: anchor.web3.Keypair;
+  beforeEach(async () => {
+    tweet = anchor.web3.Keypair.generate();
   })
 
   it('can send a new tweet', async () => {
     // Call the "SendTweet" instruction.
     await program.rpc.sendTweet('veganism', 'Hummus, am I right?', {
-        accounts: {
-            tweet: tweet.publicKey,
-            author: program.provider.wallet.publicKey,
-            systemProgram: anchor.web3.SystemProgram.programId,
-        },
-        signers: [tweet],
-    });
-
-    // Fetch the account details of the created tweet.
-    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
-  	console.log(tweetAccount);
-
-    //Ensure it has the right data
-    assert.strictEqual(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58())
-    assert.strictEqual(tweetAccount.topic , 'veganism')
-    assert.strictEqual(tweetAccount.content , 'Hummus, am I right?')
-    assert.ok(tweetAccount.timestamp)
-  });
-
-  it ("can send a new tweet without a topic", async () => {
-  //Call the SendTweet instruction
-    await program.rpc.sendTweet('', 'Hello!', {
-    accounts: {
+      accounts: {
         tweet: tweet.publicKey,
         author: program.provider.wallet.publicKey,
         systemProgram: anchor.web3.SystemProgram.programId,
-     },
-     signers: [tweet],
+      },
+      signers: [tweet],
+    });
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+    console.log(tweetAccount);
+
+    //Ensure it has the right data
+    assert.strictEqual(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58())
+    assert.strictEqual(tweetAccount.topic, 'veganism')
+    assert.strictEqual(tweetAccount.content, 'Hummus, am I right?')
+    assert.ok(tweetAccount.timestamp)
+  });
+
+  it("can send a new tweet without a topic", async () => {
+    //Call the SendTweet instruction
+    await program.rpc.sendTweet('', 'Hello!', {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweet],
     });
     // Fetch the account details of the created tweet.
     const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
     console.log(tweetAccount);
- 
+
     //Ensure it has the right data
     assert.strictEqual(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58())
-    assert.strictEqual(tweetAccount.topic , '')
-    assert.strictEqual(tweetAccount.content , 'Hello!')
+    assert.strictEqual(tweetAccount.topic, '')
+    assert.strictEqual(tweetAccount.content, 'Hello!')
     assert.ok(tweetAccount.timestamp)
   })
 
-  it ("can send a new tweet from a different author", async () => {
+  it("can send a new tweet from a different author", async () => {
     //Create new user
     const otherUser = anchor.web3.Keypair.generate();
-    
+
     //Requesting airdrop to get some lamports in the otherUser account
-    const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey,1000000000)
+    const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000)
     await program.provider.connection.confirmTransaction(signature)
-    
+
     //Call the SendTweet instruction
     await program.rpc.sendTweet('veganism', 'Yay Tofu!', {
-    accounts: {
-      tweet: tweet.publicKey,
-      author: otherUser.publicKey,
-      systemProgram: anchor.web3.SystemProgram.programId,
-    },
-    signers: [tweet,otherUser],
+      accounts: {
+        tweet: tweet.publicKey,
+        author: otherUser.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweet, otherUser],
     });
     // Fetch the account details of the created tweet.
     const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
     console.log(tweetAccount);
-   
+
     //Ensure it has the right data
     assert.strictEqual(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58())
-    assert.strictEqual(tweetAccount.topic , 'veganism')
-    assert.strictEqual(tweetAccount.content , 'Yay Tofu!')
+    assert.strictEqual(tweetAccount.topic, 'veganism')
+    assert.strictEqual(tweetAccount.content, 'Yay Tofu!')
     assert.ok(tweetAccount.timestamp)
-    })
+  })
 });
