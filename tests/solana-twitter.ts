a3c7dfe7929b9b2be5cc4c846f35162c6f1daf86
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-06 13:30:52 +0530

    test: add a new test - check it can send a new tweet without a topic

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -8,10 +8,13 @@ describe("solana-twitter", () => {
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
+  let tweet:anchor.web3.Keypair;
+  beforeEach( async()=>{
+     tweet = anchor.web3.Keypair.generate();
+  })
 
   it('can send a new tweet', async () => {
     // Call the "SendTweet" instruction.
-    const tweet = anchor.web3.Keypair.generate();
     await program.rpc.sendTweet('veganism', 'Hummus, am I right?', {
         accounts: {
             tweet: tweet.publicKey,
@@ -30,5 +33,26 @@ describe("solana-twitter", () => {
     assert.strictEqual(tweetAccount.topic , 'veganism')
     assert.strictEqual(tweetAccount.content , 'Hummus, am I right?')
     assert.ok(tweetAccount.timestamp)
-});
+  });
+
+  it ("can send a new tweet without a topic", async () => {
+  //Call the SendTweet instruction
+    await program.rpc.sendTweet('', 'Hello!', {
+    accounts: {
+        tweet: tweet.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+     },
+     signers: [tweet],
+    });
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+    console.log(tweetAccount);
+ 
+    //Ensure it has the right data
+    assert.strictEqual(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58())
+    assert.strictEqual(tweetAccount.topic , '')
+    assert.strictEqual(tweetAccount.content , 'Hello!')
+    assert.ok(tweetAccount.timestamp)
+  })
 });
